a5f42ffc2b40bb52c80a181d8834178cdf8dbbc2
Seibe TAKAHASHI <t@seibe.jp>
2021-01-30 19:42:27 +0900

    feat: IEquatableインターフェースの実装 #15

--- Assets/_Main/Scripts/Runtime/Block.cs
+++ Assets/_Main/Scripts/Runtime/Block.cs
@@ -1,7 +1,7 @@
 #nullable enable
 namespace Game
 {
-    public readonly struct Block
+    public readonly struct Block : System.IEquatable<Block>
     {
         public readonly char Value;
         public readonly bool IsFixed;
@@ -12,6 +12,29 @@ namespace Game
             Value = value;
             IsFixed = isFixed;
             IsFreeze = isFreeze;
+        }
+
+        public static bool operator !=(Block left, Block right)
+            => !left.Equals(right);
+
+        public static bool operator ==(Block left, Block right)
+            => left.Equals(right);
+
+        public bool Equals(Block other)
+                            => Value == other.Value
+            && IsFixed == other.IsFixed
+            && IsFreeze == other.IsFreeze;
+
+        public override bool Equals(object obj)
+            => (obj is Block other) && Equals(other);
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1590736083;
+            hashCode = hashCode * -1521134295 + Value.GetHashCode();
+            hashCode = hashCode * -1521134295 + IsFixed.GetHashCode();
+            hashCode = hashCode * -1521134295 + IsFreeze.GetHashCode();
+            return hashCode;
         }
     }
 }
