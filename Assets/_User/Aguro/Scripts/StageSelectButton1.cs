432031cabc27a055d9a252d60e63316ad7c4b353
aguroshou <aguroshou0413@gmail.com>
2021-01-31 13:39:09 +0900

    feat: スキル・クリア・メニューのオーバーレイを追加しました。

---
+++ Assets/_User/Aguro/Scripts/StageSelectButton1.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace User.Aguro
+{
+    public class ContinueButton : MonoBehaviour
+    {
+        [SerializeField] private int continueStageNumber;
+        [SerializeField] private string sceneName;
+        private Button button;
+        // Start is called before the first frame update
+        void Start()
+        {
+            button = GetComponent <Button>();
+            button.onClick.AddListener (OnClickButton);
+        }
+
+        public void OnClickButton()
+        {
+            //TODO: continueStageNumberを代入する
+            Debug.Log(continueStageNumber);
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
